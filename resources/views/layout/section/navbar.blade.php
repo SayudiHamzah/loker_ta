0f9e3ad00a709d42577c1a1359040fca73bc66f2
Sahaa <85856182+SayudiHamzah@users.noreply.github.com>
2025-08-29 21:42:42 +0700

    update1

--- resources/views/layout/section/navbar.blade.php
+++ resources/views/layout/section/navbar.blade.php
@@ -1,9 +1,10 @@
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
 
-        <span class="brand-text font-weight-light center">  E-Locker RC4</span>
+        <span class="brand-text font-weight-dark center">  E-Locker RC4</span>
+
     </a>
 
     <!-- Sidebar -->
